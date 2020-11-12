ff8bdaf69d6b7f5abfb0b752e3cbf1f1236a4668
adoblado <a.dob.rep@iesmarquesdecomares.org>
2020-11-12 13:05:00 +0100

    commit con bootstra

--- puntoCritico/includes/footer.php
+++ puntoCritico/includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
   <div class="container">
-    <p>&copy; <?php print date("Y");?> Alberto de Alarcón Sánchez.</p>
+    <p>&copy; <?php print date("Y");?> ChillPocket.</p>
   </div>
 </footer>
--- puntoCritico/includes/navigation.php
+++ puntoCritico/includes/navigation.php
@@ -1,3 +1,65 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <!-- Para introducir el logo-->
+        <a class="navbar-brand" href="#">
+            <img src="media/LogoSinFondo.png" alt="Logo" id="imgNav">
+        </a>
+        <!-- Para colapsar el menú en un botón al disminuir la pantalla en dispositivos pequeños Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Lo siguiente se mostrará colapsado en dispositivos pequeños (navbar-expand-md) -->
+        <div class="navbar-collapse collapse" id="collapsingNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+					Películas</a>
+					<div class="dropdown-menu bg-dark ">
+                        <a class="dropdown-item text-light" href="#">Acción</a>
+                        <a class="dropdown-item" href="#">Animación</a>
+                        <a class="dropdown-item" href="#">Aventuras</a>
+						<a class="dropdown-item" href="#">Bélico</a>
+                        <a class="dropdown-item" href="#">Comedia</a>
+                        <a class="dropdown-item" href="#">Ciencia Ficción</a>
+						<a class="dropdown-item" href="#">Drama</a>
+                        <a class="dropdown-item" href="#">Infantil</a>
+                        <a class="dropdown-item" href="#">Terror</a>
+						<a class="dropdown-item" href="#">Musical</a>
+                        <a class="dropdown-item" href="#">Romántica</a>
+                        <a class="dropdown-item" href="#">Documental</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Item 2</a>
+                </li>
+
+                <!-- Menú desplegable -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                        Item 3(tipo dropdown)
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Item 3.1</a>
+                        <a class="dropdown-item" href="#">Item 3.2</a>
+                        <a class="dropdown-item" href="#">Item 3.3</a>
+                    </div>
+                </li>
+            </ul>
+            <!--Búsqueda-->
+            <div class="ml-auto">
+                <!--La clase ml-auto "empuja a los demás elementos a la izquierda"-->
+				<!-- <form class="form-inline" action="/action_page.php">
+                    <input class="form-control m-2" type="text" placeholder="Search">
+                    <button class="btn btn-success" type="submit">Buscar</button>
+                </form>-->
+				<button class="btn btn-success" type="submit">Login</button>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+<!--
 <nav class="main-nav">
   <div class="container">
     <ul>
@@ -10,6 +72,7 @@
   </ul>
   </div>
 </nav>
+-->
 <!--
 <div class="container">
 	<ul class="nav nav-pills">
