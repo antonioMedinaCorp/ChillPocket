62660d3feff0eb820181ebe0d126de0b8f108066
adoblado <a.dob.rep@iesmarquesdecomares.org>
2020-11-16 14:04:56 +0100

    footer

--- puntoCritico/includes/footer.php
+++ puntoCritico/includes/footer.php
@@ -1,5 +1,10 @@
 <footer class="footer">
   <div class="container">
     <p>&copy; <?php print date("Y");?> ChillPocket.</p>
+    <i class="fab fa-twitter"></i>   
+    <i class="fab fa-instagram"></i>
+    <i class="fab fa-facebook-square" aria-hidden="true"></i>
+    <i class="fas fa-share-alt"></i>
+
   </div>
 </footer>
