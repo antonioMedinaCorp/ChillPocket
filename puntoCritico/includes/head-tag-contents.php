7d0b24464176d6b9c605a94a48f1f5483c359d58
nuriaRZCorp <n.rey.zam@iesmarquesdecomares.org>
2020-11-17 12:20:16 +0100

    carousel

--- puntoCritico/includes/head-tag-contents.php
+++ puntoCritico/includes/head-tag-contents.php
@@ -28,6 +28,10 @@
         integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
         crossorigin="anonymous"></script>
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 
 <link rel="stylesheet" href="css/mystyle.css">
 
