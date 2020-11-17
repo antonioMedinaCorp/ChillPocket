be26f89f97a15e964145aebd8c096fb8f1dfdfff
nuriaRZCorp <n.rey.zam@iesmarquesdecomares.org>
2020-11-17 12:36:25 +0100

    carousel

--- puntoCritico/includes/design-top.php
+++ puntoCritico/includes/design-top.php
@@ -3,4 +3,35 @@
       <h1 class="site-title">Super Cool PHP Website!</h1>
     <span class="site-tagline">Because flexbox and php is super cool!</span>
   </div>
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://picsum.photos/id/237/200/300" alt="Los Angeles" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="https://picsum.photos/id/237/200/300" alt="Chicago" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="https://picsum.photos/id/237/200/300" alt="New York" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
 </header>
+
