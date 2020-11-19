6e0b4dbe3897506058de53bc1392f0165df33149
amedrod609 <antmedrod@gmail.com>
2020-11-19 10:46:13 +0100

    fix footer

--- puntoCritico/includes/footer.php
+++ puntoCritico/includes/footer.php
@@ -1,14 +1,13 @@
 <footer class="footer bg-dark">
   <div class="container">
-<<<<<<< Updated upstream
     <p>&copy; <?php print date("Y");?> ChillPocket.</p>
     <i class="fab fa-twitter"></i>   
     <i class="fab fa-instagram"></i>
     <i class="fab fa-facebook-square" aria-hidden="true"></i>
     <i class="fas fa-share-alt"></i>
 
-=======
+
     <p>&copy; <?php print date("Y");?>ChillPocket</p>
->>>>>>> Stashed changes
+
   </div>
 </footer>
