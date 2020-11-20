4bdef8d8f9d2ae390b0cf1cc8beb8015c5149f68
adoblado <a.dob.rep@iesmarquesdecomares.org>
2020-11-21 00:23:50 +0100

    footer iconos un poco mas grandes

--- puntoCritico/includes/footer.php
+++ puntoCritico/includes/footer.php
@@ -1,11 +1,12 @@
 <footer class="footer bg-dark">
   <div class="container">
 
+    
+    <i class="fab fa-twitter fa-2x"></i>   
+    <i class="fab fa-instagram  fa-2x"></i>
+    <i class="fab fa-facebook-square  fa-2x" aria-hidden="true"></i>
+    <i class="fas fa-share-alt  fa-2x"></i>
     <p id="center">&copy; <?php print date("Y");?> ChillPocket.</p>
-    <i class="fab fa-twitter"></i>   
-    <i class="fab fa-instagram"></i>
-    <i class="fab fa-facebook-square" aria-hidden="true"></i>
-    <i class="fas fa-share-alt"></i>
 
   </div>
 </footer>
