53cda17fec9230d1afd01f840c09321ca4ad9852
adoblado <a.dob.rep@iesmarquesdecomares.org>
2020-11-22 21:06:56 +0100

    arreglo footer

--- puntoCritico/includes/footer.php
+++ puntoCritico/includes/footer.php
@@ -16,11 +16,30 @@
       </li>
     </ul>
   </div>
+
   <div class="container">
-    <i class="fab fa-twitter fa-2x"></i>
-    <i class="fab fa-instagram  fa-2x"></i>
-    <i class="fab fa-facebook-square  fa-2x" aria-hidden="true"></i>
-    <i class="fas fa-share-alt  fa-2x"></i>
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a href="nav-link">
+          <i class="fab fa-twitter fa-2x"></i>
+        </a>
+      </li>
+      <li class="nav-item"  id="withMargin">
+        <a href="nav-link">
+         <i class="fab fa-instagram  fa-2x"></i>
+        </a>
+      </li>
+      <li class="nav-item"  id="withMargin">
+        <a href="nav-link">
+          <i class="fab fa-facebook-square  fa-2x" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item"  id="withMargin">
+        <a href="nav-link">
+          <i class="fas fa-share-alt  fa-2x"></i>
+        </a>
+      </li>
+    </ul>
     <p id="center">&copy; <?php print date("Y"); ?> ChillPocket.</p>
   </div>
 
