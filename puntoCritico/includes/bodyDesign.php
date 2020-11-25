830f267305a8b7eb3c389e32322812d36d67c2d4
adoblado <a.dob.rep@iesmarquesdecomares.org>
2020-11-25 10:35:31 +0100

    con enlaces

--- puntoCritico/includes/bodyDesign.php
+++ puntoCritico/includes/bodyDesign.php
@@ -18,39 +18,45 @@
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/peli2.jpg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de película</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="theJoker.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/peli2.jpg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de película</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/peli1.jpg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de película</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="theJoker.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/peli1.jpg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de película</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/peli2.jpg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de película</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="theJoker.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/peli2.jpg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de película</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
     </div>
@@ -61,15 +67,17 @@
     <div class="row">
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/seri1.jpg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de la serie</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="theUmbrellaAc.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/seri1.jpg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de la serie</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-3 p-3">
@@ -99,15 +107,17 @@
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/serie2.jpg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de la serie</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="theUmbrellaAc.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/serie2.jpg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">The Umbrella Academy</h5>
+                            <p class="card-text">Hermanos con poderes?</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
     </div>
@@ -132,39 +142,45 @@
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/comic2.jpeg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de la serie</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="theKingdom.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/comic2.jpeg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de la serie</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/comic.png alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de la serie</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="theKingdom.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/comic.png alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Kingdom Coming</h5>
+                            <p class="card-text">El legado de los héroes</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/comic2.jpeg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de la serie</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="theKingdom.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/comic2.jpeg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de la serie</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
     </div>
@@ -189,39 +205,45 @@
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de la serie</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="principito.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de la serie</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/libro1.jpg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de la serie</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="principito.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/libro1.jpg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de la serie</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-3 p-3">
-            <div class="card text-white text-bold">
-                <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
-                <div class="card-img-overlay d-flex flex-column">
-                    <div class="mt-auto">
-                        <h5 class="card-title">Título de la serie</h5>
-                        <p class="card-text">Subtítulo</p>
+            <a href="principito.php">
+                <div class="card text-white text-bold">
+                    <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <div class="mt-auto">
+                            <h5 class="card-title">Título de la serie</h5>
+                            <p class="card-text">Subtítulo</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
 
     </div>
