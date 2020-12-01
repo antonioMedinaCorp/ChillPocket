b7cd0392d093c55d89c2d44444c635a11f1d145c
antonioMedinaCorp <amedrod609@iesmarquesdecomares.org>
2020-12-01 13:18:48 +0100

    add link to some cards and title to jumbo on umbrellaAc

--- puntoCritico/libros.php
+++ puntoCritico/libros.php
@@ -3,21 +3,207 @@
 <html>
 
 <head>
-  <?php include("includes/head-tag-contents.php"); ?>
+    <?php include("includes/head-tag-contents.php"); ?>
 </head>
 
 <body>
 
-  <?php include("includes/navigation2.php"); ?>
-  <div class="container">
-    <div class="row mt-3">
-      <div class="col-12">
-        <h2>Libros</h2>
-      </div>
-    </div>
+    <?php include("includes/navigation2.php"); ?>
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col-12">
+                <h2>Libros</h2>
+            </div>
+        </div>
 
-    <!-- fila con 4 columnas -->
-    <div class="row">
+        <!-- fila con 4 columnas -->
+        <div class="row">
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/portadaPrincipito(2).jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">El principito</h5>
+                                <p class="card-text">¿A qué esperar para leerlo?</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro1.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+        
+        <!-- fila con 4 columnas -->
+        <div class="row">
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/portadaPrincipito(2).jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">El principito</h5>
+                                <p class="card-text">¿A qué esperar para leerlo?</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro1.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+        
+        <!-- fila con 4 columnas -->
+        <div class="row">
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/portadaPrincipito(2).jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">El principito</h5>
+                                <p class="card-text">¿A qué esperar para leerlo?</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro1.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 p-3">
+                <a href="principito.php">
+                    <div class="card text-white text-bold">
+                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title">Título de la serie</h5>
+                                <p class="card-text">Subtítulo</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+        
+        <!-- fila con 4 columnas -->
+        <div class="row">
 
             <div class="col-md-3 p-3">
                 <a href="principito.php">
@@ -78,196 +264,11 @@
         </div>
     </div>
 
-    <!-- fila con 4 columnas -->
-    <div class="row">
 
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/portadaPrincipito(2).jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">El principito</h5>
-                                <p class="card-text">¿A qué esperar para leerlo?</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
 
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título de la serie</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro1.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título de la serie</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título de la serie</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-        </div>
     </div>
-    <!-- fila con 4 columnas -->
-    <div class="row">
 
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/portadaPrincipito(2).jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">El principito</h5>
-                                <p class="card-text">¿A qué esperar para leerlo?</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título del libro</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro1.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título del libro</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título del libro</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-        </div>
-    </div>
-    <!-- fila con 4 columnas -->
-    <div class="row">
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/portadaPrincipito(2).jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">El principito</h5>
-                                <p class="card-text">¿A qué esperar para leerlo?</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título de la serie</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro1.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título de la serie</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3 p-3">
-                <a href="principito.php">
-                    <div class="card text-white text-bold">
-                        <img class="card-img-top img-fluid" src=media/images/libro2.jpg alt="">
-                        <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                                <h5 class="card-title">Título de la serie</h5>
-                                <p class="card-text">Subtítulo</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-        </div>
-    </div>
-    <!-- fila con 4 columnas -->
-    
-
-
-  </div>
-
-  <?php include("includes/footer.php"); ?>
+    <?php include("includes/footer.php"); ?>
 
 </body>
 
