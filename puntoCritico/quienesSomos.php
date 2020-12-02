2149a715a1f0078ee0f25b7b25a09b3bc85d0059
adoblado <a.dob.rep@iesmarquesdecomares.org>
2020-12-02 10:20:56 +0100

    quienes somos finiquitao

---
+++ puntoCritico/quienesSomos.php
@@ -0,0 +1,99 @@
+<?php include("includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("includes/head-tag-contents.php"); ?>
+</head>
+
+<body>
+
+    <?php include("includes/navigation2.php"); ?>
+    <div class="jumbotron" id="jumbo" style="background-image:url('/media/images/conocenosJumbo.jpeg');"> </div>
+
+    <div class="container">
+        <div class="we">
+            <div class="row">
+                <div class="heading-title text-center">
+                    <h3 class="text-uppercase">Los cradores</h3>
+                    <p class="p-top-30 half-txt">Nuestros creadores diseñan y dan contenido a esta página. Las críticas no son más que meras opiniones personales sin tabúes ni filtros y sin ánimo de ofender.</p>
+                </div>
+
+                <div class="col-md-4 col-sm-4">
+                    <div class="team-member">
+                        <div class="team-img">
+                            <img src="/media/images/Nuria2.png" alt="team member" class="img-responsive">
+                        </div>
+                        <div class="team-hover">
+                            <div class="desk">
+                                <h4>Hola mundo!!</h4>
+                                <p>Creadora y deseñadora, frontend  my life!</p>
+                            </div>
+                            <div class="s-link">
+                                <a href="#"><i class="fab fa-facebook"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-google-plus"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="team-title">
+                        <h5>Nuria Reyes</h5>
+                        <span>Cofundadora y diseñadora</span>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="team-member">
+                        <div class="team-img">
+                            <img src="/media/images/Antonio2.png" alt="team member" class="img-responsive">
+                        </div>
+                        <div class="team-hover">
+                            <div class="desk">
+                                <h4>Hi people!</h4>
+                                <p>En el backstage controlando que funcione y no decepcione!</p>
+                            </div>
+                            <div class="s-link">
+                                <a href="#"><i class="fab fa-facebook"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-google-plus"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="team-title">
+                        <h5>Antonio Medina</h5>
+                        <span>Confundador y Manager</span>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="team-member">
+                        <div class="team-img">
+                            <img src="/media/images/Annmm2.png" alt="team member" class="img-responsive">
+                        </div>
+                        <div class="team-hover">
+                            <div class="desk">
+                                <h4>Welcome to my madness</h4>
+                                <p>Creando contenido y empepándome cinematrográficamente para contaros qué me parece.</p>
+                            </div>
+                            <div class="s-link">
+                                <a href="#"><i class="fab fa-facebook"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-google-plus"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="team-title">
+                        <h5>Ana Doblado</h5>
+                        <span>Cofundadora y Manger de contenidos</span>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+    <?php include("includes/footer.php"); ?>
+
+</body>
+
+</html>
