068065866cd13413ad141ae082adb61b87352261
adoblado <a.dob.rep@iesmarquesdecomares.org>
2021-02-03 10:37:12 +0100

    cambios previos para el buttom

--- includes/a_config.php
+++ includes/a_config.php
@@ -12,4 +12,31 @@
 			$CURRENT_PAGE = "Index";
 			$PAGE_TITLE = "PuntoCrítico";
 	}
+
+	//Include Google Client Library for PHP autoload file
+require_once 'googleSign/vendor/autoload.php';
+
+//Make object of Google API Client for call Google API
+$google_client = new Google_Client();
+
+//Set the OAuth 2.0 Client ID
+$google_client->setClientId('256959504312-fmqin0qvikmov5vohvuv5adcnaqlqdrp.apps.googleusercontent.com');
+
+//Set the OAuth 2.0 Client Secret key
+$google_client->setClientSecret('pI4Ib2DYhIH1ys4RsyTErrcv');
+
+//Set the OAuth 2.0 Redirect URI
+$google_client->setRedirectUri($_SERVER['SERVER_NAME']."/index.php");
+
+//
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+//start session on web page
+session_start();
+
+$login_button = '';
+
+
 ?>
