ddc680671396dd282e1077661d4d795502c9849c
nuriaRZ <55533894+nuriaRZ@users.noreply.github.com>
2021-02-06 20:12:35 +0100

    intento de conexion con bbdd

--- model/UsuarioController.php
+++ model/UsuarioController.php
@@ -1,12 +1,12 @@
 <?php
 require_once 'Conexion.php';
-require_once '../entities/Usuario.php';
+require_once 'entities/Usuario.php';
 
     class UsuarioController{
-        public function getAllUser(){
+        public static function getAllUser(){
             try {
                 $conex = new Conexion();
-                $result = $conex->prepare("select * from usuarios");                
+                $result = $conex->prepare("select * from usuario");                
     
                 if ($result->rowCount()) {
                     $u = new Usuario();
@@ -27,22 +27,16 @@ require_once '../entities/Usuario.php';
             unset($conex);
         }
 
-        public function findUserByUsername($username){
+        public static function findUserByUsername($username){
             try {
                 $conex = new Conexion();
-                $result = $conex->query("SELECT * FROM usuarios where user_name = '$username'");
+                $result = $conex->query("SELECT * FROM usuario where user_name = '$username'");
                 if ($result->rowCount()) {
                     
-                    $u = new Usuario();
-                    while ($row = $result->fetchObject()) {                   
-                        $u->newUser($row->id, $row->user_name, $row->password, $row->name, $row->apel1, $row->apel2, $row->birthdate, $row->country, $row->cod_postal, $row->phone, $row->rol);
-    
-                        
-                        $usuarios[] = clone($u);
-                        $u = new Usuario($row->id, $row->user_name, $row->password, $row->name, $row->apel1, $row->apel2, $row->birthdate, $row->country, $row->cod_postal, $row->phone, $row->rol);
-                    }
-                   
-                    return $usuarios;
+                    $row = $result->fetchObject();
+                                      
+                    $u=new Usuario($row->id, $row->user_name, $row->password, $row->name, $row->apel1, $row->apel2, $row->birthdate, $row->country, $row->cod_postal, $row->phone, $row->rol);
+                    return $u;
                 } else
                     return false;
             } catch (PDOException $ex) {
@@ -53,36 +47,30 @@ require_once '../entities/Usuario.php';
             unset($conex);
         }
 
-        public function findUserByUsernameAndPass($username, $pass){
+        public static function findUserByUsernameAndPass($username, $pass){
             try {
                 $conex = new Conexion();
-                $result = $conex->query("SELECT * FROM usuarios where user_name = '$username' and password='$pass'");
+                $result = $conex->query("SELECT * FROM usuario where user_name = '$username' and password='$pass'");
                 if ($result->rowCount()) {
                     
-                    $u = new Usuario();
-                    while ($row = $result->fetchObject()) {                   
-                        $u->newUser($row->id, $row->user_name, $row->password, $row->name, $row->apel1, $row->apel2, $row->birthdate, $row->country, $row->cod_postal, $row->phone, $row->rol);
-    
-                        
-                        $usuarios[] = clone($u);
-                        $u = new Usuario($row->id, $row->user_name, $row->password, $row->name, $row->apel1, $row->apel2, $row->birthdate, $row->country, $row->cod_postal, $row->phone, $row->rol);
-                    }
-                   
-                    return $usuarios;
+                    $row = $result->fetchObject();
+                                      
+                        $u=new Usuario($row->id, $row->user_name, $row->password, $row->name, $row->apel1, $row->apel2, $row->birthdate, $row->country, $row->cod_postal, $row->phone, $row->rol);
+                        return $u;
                 } else
                     return false;
             } catch (PDOException $ex) {
                 $errores[] = $ex->getMessage();
-                die('Error en bbdd');
+                die($ex->getMessage());
             }
             unset($result);
             unset($conex);
         }
 
-        public function editUser(Usuario $u){
+        public static function editUser(Usuario $u){
             try {
                 $conex = new Conexion();
-                $conex->query("UPDATE usuarios SET password = '$u->password', name = '$u->name', apel1 = '$u->apel1', apel2 = '$u->apel2', birthdate = '$u->birthdate', country = '$u->country', cod_post = '$u->cod_post', phone = '$u->phone' WHERE id='$u->id'");
+                $conex->query("UPDATE usuario SET password = '$u->password', name = '$u->name', apel1 = '$u->apel1', apel2 = '$u->apel2', birthdate = '$u->birthdate', country = '$u->country', cod_post = '$u->cod_post', phone = '$u->phone' WHERE id='$u->id'");
                 
             } catch (PDOException $ex) {
                 $errores[] = $ex->getMessage();
@@ -95,7 +83,7 @@ require_once '../entities/Usuario.php';
         public static function deleteUser($id) {
             try {
                 $conex = new Conexion();
-                $result = $conex->query("DELETE FROM usuarios WHERE id='$id'");            
+                $result = $conex->query("DELETE FROM usuario WHERE id='$id'");            
             } catch (PDOException $ex) {
                 $errores[] = $ex->getMessage();
                 die('Error en bbdd');
@@ -107,7 +95,7 @@ require_once '../entities/Usuario.php';
         public static function newUser(Usuario $u){
             try {
                 $conex = new Conexion();
-                $conex->exec("INSERT INTO usuarios  VALUES ('$u->id', '$u->user_name', '$u->password', '$u->name', '$u->apel1', '$u->apel2', '$u->birthdate', '$u->country', '$u->cod_postal', '$u->phone', '$u->rol')");
+                $conex->exec("INSERT INTO usuario  VALUES ('$u->id', '$u->user_name', '$u->password', '$u->name', '$u->apel1', '$u->apel2', '$u->birthdate', '$u->country', '$u->cod_postal', '$u->phone', '$u->rol')");
                 
                 
                 
