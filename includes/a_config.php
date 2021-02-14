080e2b2a61282457231da526d4daca042dcd8e8f
antonioMedinaCorp <amedrod609@iesmarquesdecomares.org>
2021-02-14 14:18:50 +0100

    fix prepare

--- includes/a_config.php
+++ includes/a_config.php
@@ -1,17 +1,17 @@
 <?php
-	// switch ($_SERVER["SCRIPT_NAME"]) {
-	// 	case "/php-template/about.php":
-	// 		$CURRENT_PAGE = "About"; 
-	// 		$PAGE_TITLE = "About Us";
-	// 		break;
-	// 	case "/php-template/contact.php":
-	// 		$CURRENT_PAGE = "Contact"; 
-	// 		$PAGE_TITLE = "Contact Us";
-	// 		break;
-	// 	default:
-	// 		$CURRENT_PAGE = "Index";
-	// 		$PAGE_TITLE = "PuntoCrítico";
-	// }
+	 switch ($_SERVER["SCRIPT_NAME"]) {
+	 	case "/php-template/about.php":
+	 		$CURRENT_PAGE = "About"; 
+	 		$PAGE_TITLE = "About Us";
+	 		break;
+	 	case "/php-template/contact.php":
+	 		$CURRENT_PAGE = "Contact"; 
+	 		$PAGE_TITLE = "Contact Us";
+	 		break;
+	 	default:
+	 		$CURRENT_PAGE = "Index";
+	 		$PAGE_TITLE = "PuntoCrítico";
+	 }
 
 	//Include Google Client Library for PHP autoload file
 require_once 'googleSign/vendor/autoload.php';
