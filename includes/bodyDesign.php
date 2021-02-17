de71cfba4140498393280eca503e4588621ea1d2
adoblado <a.dob.rep@iesmarquesdecomares.org>
2021-02-17 21:22:48 +0100

    pruebna

--- includes/bodyDesign.php
+++ includes/bodyDesign.php
@@ -14,7 +14,7 @@
         <div class="row">
 
             <div class="col-md-3 p-3">
-                <a href="theJoker.php">
+                <a href="paginaObra.php?id=102">
                     <div class="card text-white text-bold">
                         <img class="card-img-top img-fluid" src=media/images/joker.jpg alt="">
                         <div class="card-img-overlay d-flex flex-column">
