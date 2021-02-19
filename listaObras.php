adaae5bac4f25587b3a798e131679bea30e2861a
adoblado <a.dob.rep@iesmarquesdecomares.org>
2021-02-19 21:42:41 +0100

    otro de footer

--- listaObras.php
+++ listaObras.php
@@ -25,107 +25,107 @@ $linkIndex = 0;
 
   <div class="container">
     <h2>
-    
-    <?php
+
+      <?php
       switch ($_GET['tipo']) {
         case 'pelicula':
-            echo "Películas";
-            break;
+          echo "Películas";
+          break;
         case 'serie':
-            echo "Series";
-            break;
+          echo "Series";
+          break;
         case 'comic':
-            echo "Comics";
-            break;
+          echo "Comics";
+          break;
         case 'libro';
           echo "Libros";
           break;
-    }
-    ?>
-    
+      }
+      ?>
+
     </h2>
 
     <?php
     for ($z = 0; $z < $filasTotales; $z++) {
-      if(isset($arrayPeliculas[$obraIndex])){
+      if (isset($arrayPeliculas[$obraIndex])) {
 
-      
+
     ?>
-      <div class="row mt-5">
+        <div class="row mt-5 pb-5 mb-5">
 
 
-        <div class="col-sm-12">
-          <!--Carousel Wrapper-->
-          <div id="multi-item-example<?php echo $linkIndex ?>" class="carousel slide carousel-multi-item" data-ride="carousel">
+          <div class="col-sm-12">
+            <!--Carousel Wrapper-->
+            <div id="multi-item-example<?php echo $linkIndex ?>" class="carousel slide carousel-multi-item" data-ride="carousel">
 
 
-            <!--Slides-->
-            <div class="carousel-inner" role="listbox">
+              <!--Slides-->
+              <div class="carousel-inner" role="listbox">
 
 
-              <?php
-              for ($i = 0; $i < 2; $i++) {
-                echo ' <div class="carousel-item ';
-                if ($i == 0) {
-                  echo 'active';
-                }
-                echo ' ">';
+                <?php
+                for ($i = 0; $i < 2; $i++) {
+                  echo ' <div class="carousel-item ';
+                  if ($i == 0) {
+                    echo 'active';
+                  }
+                  echo ' ">';
 
-                for ($j = 0; $j < 4; $j++) {
-                  if (isset($arrayPeliculas[$obraIndex])) {
-              ?>
+                  for ($j = 0; $j < 4; $j++) {
+                    if (isset($arrayPeliculas[$obraIndex])) {
+                ?>
 
-                    <div class="col-md-3 item" style="float:left">
-                      <a href="paginaObra.php?id=<?php echo $arrayPeliculas[$obraIndex]->id ?>">
+                      <div class="col-md-3 item" style="float:left">
+                        <a href="paginaObra.php?id=<?php echo $arrayPeliculas[$obraIndex]->id ?>">
 
-                        <div class="card text-white text-bold">
-                          <img class="card-img-top img-fluid" src=<?php echo $arrayPeliculas[$obraIndex]->imagen ?> alt="">
-                          <div class="card-img-overlay d-flex flex-column">
-                            <div class="mt-auto">
-                              <h5 class="card-title"><?php echo $arrayPeliculas[$obraIndex]->title ?></h5>
-                              <p class="card-text"><?php echo $arrayPeliculas[$obraIndex]->subtitulo ?></p>
+                          <div class="card text-white text-bold">
+                            <img class="card-img-top img-fluid" src=<?php echo $arrayPeliculas[$obraIndex]->imagen ?> alt="">
+                            <div class="card-img-overlay d-flex flex-column">
+                              <div class="mt-auto">
+                                <h5 class="card-title"><?php echo $arrayPeliculas[$obraIndex]->title ?></h5>
+                                <p class="card-text"><?php echo $arrayPeliculas[$obraIndex]->subtitulo ?></p>
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      </a>
-                    </div>
+                        </a>
+                      </div>
 
-              <?php
-                    $obraIndex++;
-                  } else {
-                    break;
+                <?php
+                      $obraIndex++;
+                    } else {
+                      break;
+                    }
                   }
+
+
+
+                  echo '</div>';
                 }
+                ?>
+
+
+              </div>
 
 
 
-                echo '</div>';
-              }
-              ?>
-
+              <a class="carousel-control-prev" href="#multi-item-example<?php echo $linkIndex ?>" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#multi-item-example<?php echo $linkIndex ?>" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
 
             </div>
-
-
-
-            <a class="carousel-control-prev" href="#multi-item-example<?php echo $linkIndex ?>" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#multi-item-example<?php echo $linkIndex ?>" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-
           </div>
-        </div>
 
-      </div>
+        </div>
 
 
     <?php
-      $linkIndex++;
-          }
+        $linkIndex++;
+      }
     }
     ?>
 
@@ -133,8 +133,10 @@ $linkIndex = 0;
 
 
   </div> <!-- DIV CONTAINER -->
+  <div>
+    <?php include("includes/footer.php"); ?>
 
-  <?php include("includes/footer.php"); ?>
+  </div>
 
 </body>
 
