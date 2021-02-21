d0a5bf8e2bb62cb332782ae2ed11a146de72d9b8
antonioMedinaCorp <amedrod609@iesmarquesdecomares.org>
2021-02-22 00:17:58 +0100

    Update ValoracionController.php

--- model/ValoracionController.php
+++ model/ValoracionController.php
@@ -4,10 +4,12 @@ require_once 'Conexion.php';
 require_once 'entities/Valoracion.php';
 require_once 'ObraController.php';
 
-class ValoracionController{
+class ValoracionController
+{
 
 
-    public static function findByID($id){
+    public static function findByID($id)
+    {
 
         try {
             $conex = new Conexion();
@@ -26,12 +28,12 @@ class ValoracionController{
             $errores[] = $exc->getMessage();
             die('Error en bbdd');
         }
-
     }
 
 
-    
-    public static function findById_obra($obra){
+
+    public static function findById_obra($obra)
+    {
 
         try {
             $conex = new Conexion();
@@ -50,10 +52,10 @@ class ValoracionController{
             echo $exc->getMessage();
             die('Error en bbdd');
         }
-
     }
 
-    public static function findAllValoracionesByObra($obra){
+    public static function findAllValoracionesByObra($obra)
+    {
 
         try {
             $conex = new Conexion();
@@ -62,10 +64,10 @@ class ValoracionController{
 
             if ($result->rowCount()) {
 
-                while($reg = $result->fetchObject()){
+                while ($reg = $result->fetchObject()) {
                     $valoracion = new Valoracion($reg->id, $reg->id_usu, $reg->id_obra, $reg->point, $reg->texto);
 
-                    $ValArray[] = clone($valoracion);
+                    $ValArray[] = clone ($valoracion);
                 }
 
                 return $ValArray;
@@ -76,48 +78,46 @@ class ValoracionController{
             echo $exc->getMessage();
             die('Error en bbdd');
         }
-
-
     }
 
-    public static function findAllValoracionesByIdUsuario($id){
+    public static function findAllValoracionesByIdUsuario($id)
+    {
 
         try {
             $conex = new Conexion();
             $result = $conex->query("select * from valoracion WHERE id_usu='$id'");
-           
-            
+
+
             if ($result->rowCount()) {
 
-                while($reg = $result->fetchObject()){
+                while ($reg = $result->fetchObject()) {
                     $valoracion = new Valoracion($reg->id, $reg->id_usu, $reg->id_obra, $reg->point, $reg->texto);
 
-                    $ValArray[] = clone($valoracion);
+                    $ValArray[] = clone ($valoracion);
                 }
 
                 return $ValArray;
             }
 
             //return $result;
-        
+
             unset($result);
             unset($conex);
         } catch (PDOException $exc) {
             echo $exc->getMessage();
-            die( $exc->getMessage());
+            die($exc->getMessage());
         }
-
-
     }
 
-    public static function setValoracion($id_usu, $id_obra, $point, $texto){
-      
+    public static function setValoracion($id_usu, $id_obra, $point, $texto)
+    {
+
         try {
             $conex = new Conexion();
             $result = $conex->prepare("insert into valoracion (id_usu, id_obra, point, texto) values (?,?,?,?)");
             $result->execute([$id_usu, $id_obra, $point, $texto]);
 
-            
+
             unset($result);
             unset($conex);
         } catch (PDOException $exc) {
@@ -126,47 +126,62 @@ class ValoracionController{
         }
     }
 
-    public static function modificarValoracion(Valoracion $valor) {
+    public static function modificarValoracion(Valoracion $valor)
+    {
         try {
             $conex = new Conexion();
             $conex->exec("UPDATE valoracion SET id_usu='$valor->id_usu', id_obra='$valor->id_obra', point='$valor->point', texto='$valor->texto' WHERE id='$valor->id'");
-            
         } catch (PDOException $ex) {
-           
+
             echo 'no inserto';
-           
-            
+
+
             //mata el programa
         }
         unset($result);
         unset($conex);
     }
 
-    public static function delete($id){
-        try{
+    public static function getMediaPointByIdObra($id_obra)
+    {
+        try {
             $conex = new Conexion();
-            $conex2 = new Conexion(); 
+            $result = $conex->prepare("select avg(point) as media from valoracion WHERE id_obra=?");
+            $result->execute([$id_obra]);
+
+            if ($result->rowCount()) {
+                $reg = $result->fetchObject();
+
+                return $reg->media;
+            }
+            unset($result);
+            unset($conex);
+        } catch (PDOException $exc) {
+            $errores[] = $exc->getMessage();
+            die('Error en bbdd');
+        }
+    }
+
+    public static function delete($id)
+    {
+        try {
+            $conex = new Conexion();
+            $conex2 = new Conexion();
             $conex3 = new Conexion();
 
             $valoracion = ValoracionController::findByID($id);
 
-            $conex -> exec("DELETE from valoracion WHERE id=" . $valoracion->id_obra);
+            $conex->exec("DELETE from valoracion WHERE id=" . $valoracion->id);
 
-            $avg = $conex2->exec('select avg(point) from valoracion where id_obra='.$valoracion->id_obra);
+            $avg = ValoracionController::getMediaPointByIdObra($valoracion->id_obra);
 
             $avgRound = round($avg);
 
-            $conex3->exec("UPDATE obra o SET `point_avg` =" . $avgRound ." WHERE (`id` =" . $valoracion->id_obra . ");");
-
-
-            
-
-            
-        }catch(PDOException $ex){
+            $conex3->exec("UPDATE obra o SET `point_avg` =" . $avgRound . " WHERE (`id` =" . $valoracion->id_obra . ");");
+        } catch (PDOException $ex) {
             die($ex->getMessage());
         }
         unset($result);
         unset($conex);
     }
-
 }
