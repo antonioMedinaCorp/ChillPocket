bdd1297738ba8d6b643a7409391af5ad3eefafb0
antonioMedinaCorp <amedrod609@iesmarquesdecomares.org>
2021-02-21 23:59:26 +0100

    Update paginaObra.php

--- paginaObra.php
+++ paginaObra.php
@@ -51,7 +51,7 @@ $valoraciones = ValoracionController::findAllValoracionesByObra($obra);
 
     <div class="row">
       <div class="col-md-12">
-      <iframe width="560" height="315" src="<?php echo $obra->video; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="<?php echo $obra->video; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
     
