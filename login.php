d164f3cac6e3ba67ea60471e169f8cdc5207f5bd
adoblado <a.dob.rep@iesmarquesdecomares.org>
2021-03-01 23:44:15 +0100

    f

--- login.php
+++ login.php
@@ -58,7 +58,7 @@ if (isset($_POST['entrar']) && !empty($_POST['username']) && !empty($_POST['pass
                   <span class="sr-only">Introducir email</span>
             </div>
             <div class="form-group">
-              <input type="password" class="form-control form-control-user" name="pass" id="pass" placeholder="Contrase&ntilde;a">
+              <input type="password" class="form-control form-control-user" name="pass" id="pass" placeholder="Contrase&ntilde;a" alt="campo para password">
               <span class="sr-only">Introducir password</span>
             </div>
             <span>
